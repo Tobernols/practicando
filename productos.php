d6b17046dc5ba3561e5ecc5a9066e12dafce6f2b
Nolberto <davilariojanolberto@gmail.com>
2018-02-23 15:42:27 -0500

    Productos

---
+++ productos.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PRODUCTOS</title>
+	<link rel="stylesheet" type="text/css" href="css/producto.css">
+</head>
+<body>
+	<main>
+	<div class="menu">
+		<header></header>
+		<input type="checkbox" id="check">
+		<label for="check" class="icon-menu">MAS OPCIONES</label>
+		<h2>Magtimus</h2>
+		<nav class="nav">
+				<ul>
+					<li><a href="Welcome1.php">Pagina Principal</a></li>
+					<li><a href="Registrar Productos.php">Registrar Productos</a></li>
+				</ul>
+		</nav>
+		<article>
+		<h1>PRODUCTOS A LA VENTA ESCOLAR 2018</h1>
+		<div class="contenedor">
+		<div class="caja">
+			<h3> Tempera </h3>
+			<img src="img/temperas.jpg" width="100px" height="140px">
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Cuaderno </h3>
+			<img src="img/cuaderno.jpg" width="100px" height="140px"> 
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		<div class="caja">
+			<h3> Titulo </h3>
+			<p>Foto</p>
+			<p>S/.5.00</p>
+			<input type="button" value="Detalle">
+		</div>
+		</div>
+		</article>
+	</div>
+	<div class="piedepagina">
+       <p style="text-align: center;"> Ubiquenos en Jr.Shashuyacu S/N Pongo de Caynarachi  Celular:947503925 </p>
+      </div>
+</main>
+</body>
+</html>
